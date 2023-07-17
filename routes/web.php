97e2f296fbcf53de35c55db72ddebf17a42bacbc
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-07-17 22:58:07 +0700

    `update` route app

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,36 @@ use Illuminate\Support\Facades\Route;
 // auth admin
 Route::get('/admin/login', function () {
     return view('admin.auth.login', [
-        'title' => 'Login'
+        'title' => 'Login | Perpus Digital'
     ]);
 });
 Route::get('/admin/forgot-password', function () {
     return view('admin.auth.forgotPassword', [
-        'title' => 'Lupa Password'
+        'title' => 'Lupa Password | Perpus Digital'
     ]);
 });
 Route::get('/admin/reset-password', function () {
     return view('admin.auth.resetPassword', [
-        'title' => 'Reset Password'
+        'title' => 'Reset Password | Perpus Digital'
     ]);
 });
 
 // admin
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard', [
-        'title' => 'Dashboard Admin',
+        'title' => 'Dashboard Admin | Perpus Digital',
         'currentNav' => 'dashboard'
+    ]);
+});
+Route::get('/admin/student', function () {
+    return view('admin.students.index', [
+        'title' => 'Data Siswa | Perpus Digital',
+        'currentNav' => 'student'
+    ]);
+});
+Route::get('/admin/classes', function () {
+    return view('admin.classes.index', [
+        'title' => 'Data Kelas | Perpus Digital',
+        'currentNav' => 'course'
     ]);
 });
