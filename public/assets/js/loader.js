6160fe17fd0c7ca3be6014b5e34143ce7872b8cc
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-07-21 05:59:32 +0700

    `update` styles empty and loading state

--- public/assets/js/loader.js
+++ public/assets/js/loader.js
@@ -1,7 +1,7 @@
 const tableLoader = (colSpan, dataImage) => {
     return `<tr>
                 <td colspan="${colSpan}" class="text-center">
-                    <div class="d-flex w-100 align-items-center justify-content-center py-3">
+                    <div class="d-flex w-100 align-items-center justify-content-center py-2">
                         <object data="${dataImage}" type="image/svg+xml" style="height: 48px !important"></object>
                     </div>
                 </td>
